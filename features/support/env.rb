4b195142693a8a378bedbe19ae871f621f17920b
PatriciaCompass <patricia.lemos@compasso.com.br>
2022-08-08 09:26:45 -0300

    Adicionando capabilities

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,4 +3,15 @@ require 'selenium-webdriver'
 require 'rspec'
 require 'cucumber'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENVIRONMENT}.yml")
+puts ENVIRONMENT_CONFIG
+URL = ENVIRONMENT_CONFIG['url']
+
+Capybara.register_driver :my_chrome do |app|
+    caps = Selenium::webdriver::Remote::Capabilities.chrome()
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, options: options)
+
+
 Capybara.default_driver = :selenium_chrome
+Capybara.app_host = URL
