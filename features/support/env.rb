763eea2589a567321f50b69796da4e6d49d69bb8
PatriciaCompass <patricia.lemos@compasso.com.br>
2022-08-10 08:56:33 -0300

    Update suporte capabilities

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,17 +12,24 @@ puts ENVIRONMENT_CONFIG
 URL = ENVIRONMENT_CONFIG['url']
 
 Capybara.register_driver :my_chrome do |app|
-    caps = Selenium::WebDriver::Remote::Cpabilities.chrome("goog:chromeOptions" => {"args" => ["--incognito",
-         "--start-maximized", "--enable-debug", "--window-size=1420,835"]})
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => {"args" => ["--incognito", 
+        "--ignore-certificates-erros", "--disable-popup-blocking", "--start-maximized", "--enable-debug",
+        "--window-size=1420,835", "--ignore-ssl-errors", "--disable-gpu","--disable-translate","--no-sandbox",
+        "--disable-impl-side-painting", "--debug_level=3", "--acceptInsecureCerts=true"]})
     
-    if ENV['HEADLESS']
+    if  ENV['HEADLESS']
        caps['goog:chromeOptions']['args'] << '--headless'
-    end        
-
+       caps['goog:chromeOptions']['args'] << '--disable-site-isolation-trials'
+    end
+    client = Selenium::WebDriver::Remote::Http::Default.new
+          
     options = { browser: :chrome, desired_capabilities: caps }
+
+    puts 'Raising driver'
     Capybara::Selenium::Driver.new(app, options)
 end
 
-Capybara.default_driver = :selenium_chrome
-Capybara.app_host = URL
+Capybara.default_driver         = :my_chrome
+Capybara.app_host               = URL
+Capybara.default_max_wait_time  = 10
 
